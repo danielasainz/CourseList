5c824494c9a8eeb889fe2ecab830438f2f78e244
Daniela Sainz <dasainz18@gmail.com>
2018-04-03 15:12:06 -0400

    project completed

--- src/main/java/com/sainz/course/MainJava.java
+++ src/main/java/com/sainz/course/MainJava.java
@@ -4,38 +4,51 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainJava {
-
     public static void main(String[] args) {
-
+        boolean done = false;
         Scanner scan = new Scanner(System.in);
+        String option;
         Course newCourse;
+
 
         ArrayList<Course> courses = new ArrayList<>();
 
+            do {
+                newCourse = new Course();
+                System.out.println("Enter your course number");
+                newCourse.setCourseNumber(scan.nextLine());
+                System.out.println("Enter your course name");
+                newCourse.setCourseName(scan.nextLine());
+                System.out.println("Enter your room number");
+                newCourse.setRoomNumber(scan.nextLine());
+                System.out.println("Enter your semester (Fall or Spring)");
+                newCourse.setCourseSemester(scan.nextLine());
+                System.out.println("Enter your year");
+                newCourse.setCourseYear(scan.nextLine());
 
-        for (int i = 1; i <= 5; i++)
-        {
-            newCourse = new Course();
-            System.out.println("Enter your course number");
-            newCourse.setCourseNumber(scan.nextLine());
-            System.out.println("Enter your course name");
-            newCourse.setCourseName(scan.nextLine());
-            System.out.println("Enter your room number");
-            newCourse.setRoomNumber(scan.nextLine());
-            System.out.println("Enter your semester (Fall or Spring)");
-            newCourse.setCourseSemester(scan.nextLine());
-            System.out.println("Enter your year");
-            newCourse.setCourseYear(scan.nextLine());
-            courses.add(newCourse);
+                courses.add(newCourse);
 
+                System.out.println("Do you want to enter the details for another course? (Y/N)");
+                option = scan.nextLine();
+                if (!option.equalsIgnoreCase("y")) {
+                    done = false;
+                } else {
+                    done = true;
+                }
+
+
+            } while(done);
+
+            for (Course eachCourse : courses)
+
+            {
+
+                System.out.println("========");
+                System.out.println("Course number:" + " " + eachCourse.getCourseNumber());
+                System.out.print("Course contents:" + " " + eachCourse.toString());
+            }
         }
-        for (Course eachCourse : courses)
-
-        {
-            System.out.println("========");
-            System.out.println("Course number:" + " " + eachCourse.getCourseNumber());
-            System.out.print("Course contents:" + " " + eachCourse.toString());
-        }
-
-    }
 }
+
+
+
